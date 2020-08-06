d30712d144ef036e01e3c5e45e92fe7ddb52fe29
zhengfei <zhengfei@innotechx.com>
2020-08-06 18:24:45 +0800

    commint3

--- src/com/test/hello/Test.java
+++ src/com/test/hello/Test.java
@@ -21,6 +21,7 @@ public class Test {
     public static void main(String[] args) {
         1
                 2
+                        3
 
 //        LintImage lintImage = new LintImage(
 //                "E:\\Working3\\Air\\app\\Module\\air_purifier_131",
